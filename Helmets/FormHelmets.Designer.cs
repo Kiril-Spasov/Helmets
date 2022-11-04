4235bc5d438ecd4e66c9293468a761c88a4690e0
Kiril Spasov <spasov.kiril@gmail.com>
2022-11-04 12:09:39 +0200

    Add project files.

---
+++ Helmets/FormHelmets.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Helmets
+{
+    partial class FormHelmets
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.LstResult = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Location = new System.Drawing.Point(85, 75);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(226, 67);
+            this.BtnStart.TabIndex = 0;
+            this.BtnStart.Text = "Start Simulation";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // LstResult
+            // 
+            this.LstResult.FormattingEnabled = true;
+            this.LstResult.ItemHeight = 16;
+            this.LstResult.Location = new System.Drawing.Point(422, 77);
+            this.LstResult.Name = "LstResult";
+            this.LstResult.Size = new System.Drawing.Size(328, 308);
+            this.LstResult.TabIndex = 1;
+            // 
+            // FormHelmets
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LstResult);
+            this.Controls.Add(this.BtnStart);
+            this.Name = "FormHelmets";
+            this.Text = "Helmets Simulation";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.ListBox LstResult;
+    }
+}
+
---
+++ Helmets/FormHelmets.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Helmets
+{
+    public partial class FormHelmets : Form
+    {
+        public FormHelmets()
+        {
+            InitializeComponent();
+        }
+
+        /*
+         NFL HELMETS PROBLEM
+        There are 28 football teams in the NFL. Many supermarkets and discount stores have vending machines 
+        that dispense miniature team helmets for one quarter (25 cents) each. Assume that all helmets are equally likely 
+        to be dispensed by the machines. You are to write a program to simulate putting quarters in vending machines 
+        until all 28 helmets have been obtained. Your output should have the form : TOTAL SPENT TO GET ALL 28 
+        HELMETS =
+        Run your simulation 10 times and also determine the average total spent.
+         */
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            LstResult.Items.Clear();
+            Random r = new Random();
+            int hPicked = 0;
+            double costsPerSimulation = 0;
+            double totalCosts = 0;
+            int[] helmetFlag = new int[29];
+
+            for (int simulation = 1; simulation <= 10; simulation++)
+            {
+                ClearArray(helmetFlag);
+
+                for (int i = 1; i <= 28; i++)
+                {
+                    do
+                    {
+                        costsPerSimulation += 0.25;
+                        hPicked = r.Next(1, 29);
+                    }
+                    while (helmetFlag[hPicked] == 1);
+
+                    helmetFlag[hPicked] += 1;
+                }
+                LstResult.Items.Add("Simulation " + simulation);
+                LstResult.Items.Add("Total spent to get all 28 helmets: " + costsPerSimulation);
+                totalCosts += costsPerSimulation;
+            }
+            LstResult.Items.Add(Environment.NewLine);
+            LstResult.Items.Add("Average costs for 10 simulations is: " + (totalCosts / 10)).ToString("n2");
+        }
+
+        private void ClearArray(int[] ar)
+        {
+            for (int i = 1; i <= ar.Length - 1; i++)
+            {
+                ar[i] = 0;
+            }
+        }
+    }
+}
